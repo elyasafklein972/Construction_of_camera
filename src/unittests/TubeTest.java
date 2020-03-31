e803c9603c0eb36163eefb37c37d14e187754aa8
omerhad <omerhadad23@gmail.com>
2020-03-31 08:49:35 +0300

    31/03/2020 08:50

--- src/unittests/TubeTest.java
+++ src/unittests/TubeTest.java
@@ -23,23 +23,17 @@ public class TubeTest {
         public void getNormal() {
             Ray _r=new Ray(new Point3D(1,1,1),new Vector(1,2,3));
             Tube _t=new Tube(2,_r);
-            Vector v=new Vector(1,2,3);
-            Point3D p =new Point3D(1,1,1);
+
             Point3D p2=new Point3D(1,2,3);
-            Vector vector1 = p2.subtract(p);
 
-            //We need the projection to multiply the _direction unit vector
-            double projection = vector1.dotProduct(v);
-            if(!isZero(projection))
-            {
-                // projection of P-O on the ray:
-                p.add(v.scale(projection));
-            }
+            //The Normal vector expected
+            Vector c2=new Vector(0.0,0.4472135954999579,0.8944271909999159);
+
+            assertEquals(c2,_t.getNormal(p2));
 
 
-            Vector c = p2.subtract(p);
-            
-            assertEquals(c.normalize(),_t.getNormal(p2));
+
+
         }
 
         @Test
