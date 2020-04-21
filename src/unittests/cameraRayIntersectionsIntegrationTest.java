e7f111ba6a95380386b51eeaa972e192e52bc4c3
elyasaf klein <elyasafklein@gmail.com>
2020-04-21 20:09:48 +0300

    part of targil 4 21/4/20 20:09

--- src/unittests/cameraRayIntersectionsIntegrationTest.java
+++ src/unittests/cameraRayIntersectionsIntegrationTest.java
@@ -1,5 +1,6 @@
 package unittests;
 import elements.Camera;
+import geometries.Plane;
 import geometries.Sphere;
 import org.junit.Test;
 import primitives.Point3D;
@@ -21,22 +22,144 @@ public class cameraRayIntersectionsIntegrationTest {
 
 
     @Test
-    public void cameraRaySphereIntegrationTest() {
-        Camera cam1 = new Camera(Point3D.ZERO, new Vector(0, 0, 1), new Vector(0, -1, 0));
-        Camera cam2 = new Camera(new Point3D(0, 0, -0.5), new Vector(0, 0, 1), new Vector(0, -1, 0));
-        Sphere sphere;
-        List<Point3D> intersections=new ArrayList<Point3D>() ;
-        int count;
+    public void cameraRaySphereIntegrationTest1() {
+        Camera cam = new Camera(Point3D.ZERO, new Vector(0, 0, 1), new Vector(0, -1, 0));
+        List<Point3D> intersections=new ArrayList<Point3D>();
+        Sphere sphere = new Sphere(1, new Point3D(0, 0, 3));
+        int count=0;
 
-        sphere = new Sphere(1, new Point3D(0, 0, 3));
-        count =0;
-        for (int i=0;i<3;i++)
-        for(int j=0;j<3;j++){
-            intersections=sphere.findIntersections(cam1.constructRayThroughPixel(3,3,j,i,1,3,3));
+        for (int i=0;i<3;i++) {
+            for (int j = 0; j < 3; j++) {
+                intersections = sphere.findIntersections(cam.constructRayThroughPixel(3, 3, j, i, 1, 3, 3));
+                if (intersections != null)
+                count+=intersections.size();
+            }
         }
-    if (intersections != null)
-    count+=intersections.size();
-        assertEquals("wronge intersection",2,count);
 
+    if (intersections != null)
+        assertEquals("wronge intersection",2,count);
     }
+
+
+
+    @Test
+    public void cameraRaySphereIntegrationTest2() {
+        Camera cam = new Camera(new Point3D(0, 0, -0.5), new Vector(0, 0, 1), new Vector(0, -1, 0));
+        Sphere sphere = new Sphere(2.5, new Point3D(0, 0, 2.5));
+       List<Point3D> intersections=new ArrayList<Point3D>();
+        int count = 0;
+
+
+        for (int i=0;i<3;i++) {
+            for (int j = 0; j < 3; j++) {
+                intersections = sphere.findIntersections(cam.constructRayThroughPixel(3, 3, j, i, 1, 3, 3));
+                if (intersections != null)
+                    count+=intersections.size();
+            }
+        }
+
+        if (intersections != null)
+            assertEquals("wronge intersection",18,count);
+    }
+
+
+    @Test
+    public void cameraRaySphereIntegrationTest3() {
+        Camera cam = new Camera(new Point3D(0, 0, -0.5), new Vector(0, 0, 1), new Vector(0, -1, 0));
+        Sphere sphere = new Sphere(2, new Point3D(0, 0, 2));
+        List<Point3D> intersections=new ArrayList<Point3D>();
+        int count = 0;
+
+
+        for (int i=0;i<3;i++) {
+            for (int j = 0; j < 3; j++) {
+                intersections = sphere.findIntersections(cam.constructRayThroughPixel(3, 3, j, i, 1, 3, 3));
+                if (intersections != null)
+                    count+=intersections.size();
+            }
+        }
+
+        if (intersections != null)
+            assertEquals("wronge intersection",10,count);
+    }
+
+    @Test
+    public void cameraRaySphereIntegrationTest4() {
+        Camera cam = new Camera(new Point3D(2, 2, 2), new Vector(0, 0, 1), new Vector(0, -1, 0));
+        Sphere sphere = new Sphere(4, new Point3D(2, 2, 2));
+        List<Point3D> intersections=new ArrayList<Point3D>();
+        int count = 0;
+
+
+        for (int i=0;i<3;i++) {
+            for (int j = 0; j < 3; j++) {
+                intersections = sphere.findIntersections(cam.constructRayThroughPixel(3, 3, j, i, 1, 3, 3));
+                if (intersections != null)
+                    count+=intersections.size();
+            }
+        }
+
+        if (intersections != null)
+            assertEquals("wronge intersection",9,count);
+    }
+
+    @Test
+    public void cameraRaySphereIntegrationTest5() {
+        Camera cam = new Camera(Point3D.ZERO, new Vector(0, 0, 1), new Vector(0, -1, 0));
+        Sphere sphere = new Sphere(0.5, new Point3D(0, 0, -1));
+        List<Point3D> intersections=new ArrayList<Point3D>();
+        int count = 0;
+
+
+        for (int i=0;i<3;i++) {
+            for (int j = 0; j < 3; j++) {
+                intersections = sphere.findIntersections(cam.constructRayThroughPixel(3, 3, j, i, 1, 3, 3));
+                if (intersections != null)
+                    count+=intersections.size();
+            }
+        }
+
+        if (intersections != null)
+            assertEquals("wronge intersection",0,count);
+    }
+
+    @Test
+    public void cameraRayPlaneIntegrationTest1(){
+        Camera cam = new Camera(Point3D.ZERO, new Vector(0, 0, 1), new Vector(0, -1, 0));
+        Plane plane = new Plane(new Point3D(0,0,4),new Vector(0,0,1));
+        List<Point3D> intersections=new ArrayList<Point3D>();
+        int count = 0;
+
+
+        for (int i=0;i<3;i++) {
+            for (int j = 0; j < 3; j++) {
+                intersections = plane.findIntersections(cam.constructRayThroughPixel(3, 3, j, i, 1, 3, 3));
+                if (intersections != null)
+                    count+=intersections.size();
+            }
+        }
+        if (intersections != null)
+            assertEquals("wronge intersection",9,count);
+    }
+
+    @Test
+    public void cameraRayPlaneIntegrationTest2(){
+        Camera cam = new Camera(Point3D.ZERO, new Vector(0, 0, 1), new Vector(0, -1, 0));
+        Plane plane = new Plane(new Point3D(0,0,4),new Vector(2,2,0));
+        List<Point3D> intersections=new ArrayList<Point3D>();
+        int count = 0;
+
+
+        for (int i=0;i<3;i++) {
+            for (int j = 0; j < 3; j++) {
+                intersections = plane.findIntersections(cam.constructRayThroughPixel(3, 3, j, i, 1, 3, 3));
+                if (intersections != null)
+                    count+=intersections.size();
+            }
+        }
+        if (intersections != null)
+            assertEquals("wronge intersection",9,count);
+    }
+
+
 }
